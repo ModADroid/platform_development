df5d27dc1e43989e05af5071d92fce0025301526
jt1134 <jt1134@gmail.com>
2011-02-17 22:41:35 -0600

    remove useless options
    Change-Id: I5633503019ed0b0b003a07606a816a2b4c53784c

--- apps/SpareParts/src/com/android/spare_parts/SpareParts.java
+++ apps/SpareParts/src/com/android/spare_parts/SpareParts.java
@@ -64,13 +64,8 @@ public class SpareParts extends PreferenceActivity
     private static final String WINDOW_ANIMATIONS_PREF = "window_animations";
     private static final String TRANSITION_ANIMATIONS_PREF = "transition_animations";
     private static final String FANCY_IME_ANIMATIONS_PREF = "fancy_ime_animations";
-    private static final String HAPTIC_FEEDBACK_PREF = "haptic_feedback";
-    private static final String FONT_SIZE_PREF = "font_size";
-    private static final String END_BUTTON_PREF = "end_button";
     private static final String KEY_COMPATIBILITY_MODE = "compatibility_mode";
 
-    private final Configuration mCurConfig = new Configuration();
-    
     //extra
     private CheckBoxPreference mMenuUnlockScreenPref;
     private CheckBoxPreference mLauncherOrientationPref;
@@ -84,9 +79,6 @@ public class SpareParts extends PreferenceActivity
     private ListPreference mWindowAnimationsPref;
     private ListPreference mTransitionAnimationsPref;
     private CheckBoxPreference mFancyImeAnimationsPref;
-    private CheckBoxPreference mHapticFeedbackPref;
-    private ListPreference mFontSizePref;
-    private ListPreference mEndButtonPref;
     private CheckBoxPreference mCompatibilityMode;
 
     private IWindowManager mWindowManager;
@@ -149,11 +141,6 @@ public class SpareParts extends PreferenceActivity
         mTransitionAnimationsPref = (ListPreference) prefSet.findPreference(TRANSITION_ANIMATIONS_PREF);
         mTransitionAnimationsPref.setOnPreferenceChangeListener(this);
         mFancyImeAnimationsPref = (CheckBoxPreference) prefSet.findPreference(FANCY_IME_ANIMATIONS_PREF);
-        mHapticFeedbackPref = (CheckBoxPreference) prefSet.findPreference(HAPTIC_FEEDBACK_PREF);
-        mFontSizePref = (ListPreference) prefSet.findPreference(FONT_SIZE_PREF);
-        mFontSizePref.setOnPreferenceChangeListener(this);
-        mEndButtonPref = (ListPreference) prefSet.findPreference(END_BUTTON_PREF);
-        mEndButtonPref.setOnPreferenceChangeListener(this);
         mCompatibilityMode = (CheckBoxPreference) findPreference(KEY_COMPATIBILITY_MODE);
         mCompatibilityMode.setPersistent(false);
         mCompatibilityMode.setChecked(Settings.System.getInt(getContentResolver(),
@@ -190,9 +177,6 @@ public class SpareParts extends PreferenceActivity
         mFancyImeAnimationsPref.setChecked(Settings.System.getInt(
                 getContentResolver(), 
                 Settings.System.FANCY_IME_ANIMATIONS, 0) != 0);
-        mHapticFeedbackPref.setChecked(Settings.System.getInt(
-                getContentResolver(), 
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 0) != 0);
     }
     
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -200,10 +184,6 @@ public class SpareParts extends PreferenceActivity
             writeAnimationPreference(0, objValue);
         } else if (preference == mTransitionAnimationsPref) {
             writeAnimationPreference(1, objValue);
-        } else if (preference == mFontSizePref) {
-            writeFontSizePreference(objValue);
-        } else if (preference == mEndButtonPref) {
-            writeEndButtonPreference(objValue);
         //extra
         } else if (preference == mBatteryFontSizePref) {
             writeBatteryFontSizePreference(objValue);
@@ -246,15 +226,7 @@ public class SpareParts extends PreferenceActivity
         } catch (RemoteException e) {
         }
     }
-    
-    public void writeFontSizePreference(Object objValue) {
-        try {
-            mCurConfig.fontScale = Float.parseFloat(objValue.toString());
-            ActivityManagerNative.getDefault().updateConfiguration(mCurConfig);
-        } catch (RemoteException e) {
-        }
-    }
-    
+
     //extra
     public void writeBatteryFontSizePreference(Object objValue) {
         try {
@@ -266,15 +238,6 @@ public class SpareParts extends PreferenceActivity
     }
     //end extra
 
-    public void writeEndButtonPreference(Object objValue) {
-        try {
-            int val = Integer.parseInt(objValue.toString());
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.END_BUTTON_BEHAVIOR, val);
-        } catch (NumberFormatException e) {
-        }
-    }
-    
     int floatToIndex(float val, int resid) {
         String[] indices = getResources().getStringArray(resid);
         float lastVal = Float.parseFloat(indices[0]);
@@ -296,17 +259,7 @@ public class SpareParts extends PreferenceActivity
         } catch (RemoteException e) {
         }
     }
-    
-    public void readFontSizePreference(ListPreference pref) {
-        try {
-            mCurConfig.updateFrom(
-                ActivityManagerNative.getDefault().getConfiguration());
-        } catch (RemoteException e) {
-        }
-        pref.setValueIndex(floatToIndex(mCurConfig.fontScale,
-                R.array.entryvalues_font_size));
-    }
-    
+
     //extra
     private int readBatteryFontSizePreference(ListPreference pref) {
         try {
@@ -318,14 +271,6 @@ public class SpareParts extends PreferenceActivity
     }
     //end extra
 
-    public void readEndButtonPreference(ListPreference pref) {
-        try {
-            pref.setValueIndex(Settings.System.getInt(getContentResolver(),
-                    Settings.System.END_BUTTON_BEHAVIOR));
-        } catch (SettingNotFoundException e) {
-        }
-    }
-    
     //extra
     private int readClockFontColor() {
         try {
@@ -380,10 +325,6 @@ public class SpareParts extends PreferenceActivity
             Settings.System.putInt(getContentResolver(),
                     Settings.System.FANCY_IME_ANIMATIONS,
                     mFancyImeAnimationsPref.isChecked() ? 1 : 0);
-        } else if (HAPTIC_FEEDBACK_PREF.equals(key)) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.HAPTIC_FEEDBACK_ENABLED,
-                    mHapticFeedbackPref.isChecked() ? 1 : 0);
         }
     }
     
@@ -392,8 +333,6 @@ public class SpareParts extends PreferenceActivity
         super.onResume();
         readAnimationPreference(0, mWindowAnimationsPref);
         readAnimationPreference(1, mTransitionAnimationsPref);
-        readFontSizePreference(mFontSizePref);
-        readEndButtonPreference(mEndButtonPref);
         //extra
         readBatteryFontSizePreference(mBatteryFontSizePref);
         //end extra
